e59e0b6f5e7444ff56a3305865f3ee01528547c2
kewang <cpckewang@gmail.com>
2016-07-06 16:45:00 +0800

    Fix compile error

--- src/test/java/tw/kewang/logback/appender/AppTest.java
+++ src/test/java/tw/kewang/logback/appender/AppTest.java
@@ -3,10 +3,9 @@ package tw.kewang.logback.appender;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tw.kewang.App;
 
 public class AppTest {
-    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AppTest.class);
 
     @Test
     public void testApp() {
